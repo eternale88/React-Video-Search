ebeed091d5391e6119672786b5186609d945ef4f
eternale88 <ethanrobbins@ymail.com>
2020-02-10 20:04:56 -0500

    add file

---
+++ src/apis/youtube.js
@@ -0,0 +1,12 @@
+import axios from 'axios'
+import { YOUTUBE_API_KEY } from './key'
+
+export default axios.create({
+  baseURL: 'https://www.googleapis.com/youtube/v3',
+  params: {
+    part: 'snippet',
+    type: 'video',
+    maxResults: 5,
+    key: YOUTUBE_API_KEY
+  }
+})
