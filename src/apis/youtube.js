cdf1692aaa3ee399568da16dfb64c35fee8058ab
eternale88 <ethanrobbins@ymail.com>
2020-02-10 20:12:51 -0500

    fix

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import { YOUTUBE_API_KEY } from './key'
+// import { YOUTUBE_API_KEY } from './key'
 
 export default axios.create({
   baseURL: 'https://www.googleapis.com/youtube/v3',
